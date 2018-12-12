8ba230d4de7db185f0a99577a0742411c3dea403
Jesus Medina <chuymedina96@gmail.com>
2018-12-12 19:41:27 +0000

    adding post functionality to api endpoint

--- public/js/main.js
+++ public/js/main.js
@@ -2,15 +2,29 @@
 $(document).ready(function(){
     var url = "/api/todos";
     $.getJSON(url)
-    .then(addToDos) 
+    .then(addToDos) ;
+    $("#todoInput").keypress(function(event){
+        if(event.which == 13){
+            createToDo();
+        }
+    });
+    /*
     .catch(function(err){
         console.log(err);
-    });
+    });*/
 });
 
 function addToDos(todos){
     todos.forEach(function(todo){
-        var newToDo = $('<li class="task">' + todo.name + '</li>');
+        var newToDo = $('<li class="task">' + todo.name + todo.completed + '</li>');
+        if(todo.completed){
+            newToDo.addClass("done");
+        }
         $(".list").append(newToDo);
     });
+}
+function createToDo(){
+    usrInput = $("#todoInput").val();
+    console.log(usrInput);
+    //$.post(url, {name: usrInput });
 }
